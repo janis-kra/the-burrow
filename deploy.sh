79672b46fbad2d2c34e93cae12252df9771a765b
Janis Krasemann <janis.krasemann@enpit.de>
2026-07-03 11:34:01 +0200

    Tailscale-compatible deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o burrow ./cmd/burrow
+USER=jk
+HOST=aragorn.tail40acc0.ts.net
 
-ssh jk@aragorn.local "sudo systemctl stop burrow"
-scp burrow jk@aragorn.local:~/burrow/
-scp config.yaml jk@aragorn.local:~/burrow/
-scp -r templates/ jk@aragorn.local:~/burrow/
-ssh jk@aragorn.local "sudo systemctl start burrow"
-
+ssh $USER@$HOST "sudo systemctl stop burrow"
+scp burrow $USER@$HOST:~/burrow/
+scp config.yaml $USER@$HOST:~/burrow/
+scp -r templates/ $USER@$HOST:~/burrow/
+ssh $USER@$HOST "sudo systemctl start burrow"
